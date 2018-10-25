a6cb7329f955b72c9ab7655dc1205559fea7e23a
Nemael <panouillot.mael@gmail.com>
2018-10-25 15:43:37 +0200

    Completely working, but the file for the requests is not working, due to API key being in it

--- GUI.py
+++ GUI.py
@@ -5,101 +5,186 @@ import youtube_subs
 import urllib
 import subprocess
 import pafy
+from threading import Thread
+import time
+from cStringIO import StringIO
+import sys
+import os
+import datetime
+import random
 
+class GUI():
+	def __init__(self,url_list,show_old):
+		self.app = QApplication(sys.argv)
+		self.widget = QWidget()
+		self.widget.resize(650,800)
+		self.widget.setWindowTitle("Selection")
+		self.widget.show()
+		
+		layout = QVBoxLayout()
+		self.widget.cb = QListWidget()
+		self.widget.cb.setStyleSheet("QListWidget::item:focus{border:none;outline:none;}")
+		self.widget.cb.setStyleSheet("QListWidget::item {border-style:outset;border-width: 2px;border-color: grey;}")
+		
+		
+		self.myWidgetList = []
+		
+		for i in range(len(url_list)):
+			print(i)
+			try:
+				video = pafy.new(url_list[i])
+				month_out = int(video.published[5:7])
+				now = datetime.datetime.now()
+				if(show_old or now.month - month_out <= 2):
+					myQWidget = WidgetVideo(url_list[i],video.length)
+					self.myWidgetList.append(myQWidget)
+					myQListWidgetItem = QListWidgetItem(self.widget.cb)
+					myQListWidgetItem.setSizeHint(myQWidget.sizeHint())
+					self.widget.cb.addItem(myQListWidgetItem)
+					self.widget.cb.setItemWidget(myQListWidgetItem, myQWidget)
+			except:
+				print("Il y a eu une erreur, la video n'existe pas")
+		layout.addWidget(self.widget.cb)
+		
+		Hlayout = QHBoxLayout()
+		okButton = QPushButton("OK")
+		cancelButton = QPushButton("CANCEL")
+		selectAllButton = QPushButton("SELECT ALL")
+		deselectAllButton = QPushButton("DESELECT ALL")
+		Hlayout.addWidget(okButton)
+		Hlayout.addWidget(cancelButton)
+		Hlayout.addWidget(selectAllButton)
+		Hlayout.addWidget(deselectAllButton)
+		layout.addLayout(Hlayout)
+		
+		self.widget.setLayout(layout)
+		
+		okButton.clicked.connect(self.ok)
+		cancelButton.clicked.connect(self.cancel)
+		selectAllButton.clicked.connect(self.select_all)
+		deselectAllButton.clicked.connect(self.deselect_all)
+		
+		self.widget.cb.sortItems()
+		
+		sys.exit(self.app.exec_())
+		
+	def select_all(self):
+		for i in range(len(self.myWidgetList)):
+			self.myWidgetList[i].check.setChecked(True)
+	
+	def deselect_all(self):
+		for i in range(len(self.myWidgetList)):
+			self.myWidgetList[i].check.setChecked(False)
+	
+	def cancel(self):
+		sys.exit()
+		
+	def clearLayout(self,layout):
+		while layout.count():
+			child = layout.takeAt(0)
+			if child.widget() is not None:
+				child.widget().deleteLater()
+			elif child.layout() is not None:
+				self.clearLayout(child.layout())
 
+	def ok(self):
+		newfont = QFont("Times",22,QFont.Bold)
+		to_dl_list = []
+		ost = sys.stdout
+		for i in range(len(self.myWidgetList)):
+			if self.myWidgetList[i].check.isChecked():
+				to_dl_list.append(self.myWidgetList[i].url)
+		print(to_dl_list)
+		for i in reversed(range(self.widget.layout().count())): 
+			self.clearLayout(self.widget.layout())
+		self.progress_single = QProgressBar()
+		self.progress_group = QProgressBar()
+		self.progress_single.setValue(100)
+		self.progress_group.setValue(100)
+		
 
-def start(url_list):
-	app = QApplication(sys.argv)
-	widget = QWidget()
-	widget.resize(650,800)
-	widget.setWindowTitle("Selection")
-	widget.show()
-	
-	layout = QVBoxLayout()
-	widget.cb = QListWidget()
-	#widget.cb.setFocusPolicy(Qt.NoFocus)
-	#widget.cb.setStyleSheet("QListWidget {border-style:outset;border-width:10px;border-color:black;}")
-	widget.cb.setStyleSheet("QListWidget::item:focus{border:none;outline:none;}")
-	widget.cb.setStyleSheet("QListWidget::item {border-style:outset;border-width: 2px;border-color: grey;}")
-	
-	
-	myWidgetList = []
-	
-	for i in range(len(url_list)):
-		myQWidget = WidgetVideo(url_list[i])
-		myWidgetList.append(myQWidget)
-		myQListWidgetItem = QListWidgetItem(widget.cb)
-		myQListWidgetItem.setSizeHint(myQWidget.sizeHint())
-		widget.cb.addItem(myQListWidgetItem)
-		widget.cb.setItemWidget(myQListWidgetItem, myQWidget)
-	
-	
-	layout.addWidget(widget.cb)
-	
-	Hlayout = QHBoxLayout()
-	okButton = QPushButton("OK")
-	cancelButton = QPushButton("CANCEL")
-	Hlayout.addWidget(okButton)
-	Hlayout.addWidget(cancelButton)
-	layout.addLayout(Hlayout)
-	
-	widget.setLayout(layout)
-	
-	okButton.clicked.connect(lambda: ok(myWidgetList,widget))
-	cancelButton.clicked.connect(cancel)
-	
-	sys.exit(app.exec_())
-	
+		self.quitButton = QPushButton("Quit")
+		self.quitButton.clicked.connect(self.exit_app)
+		self.quitButton.setEnabled(False)
+		
+		self.name_label = QLabel("")
+		self.name_label.setFont(newfont)
+		self.video_total_label = QLabel("")
+		self.video_total_label.setFont(newfont)
+		self.temps_restant_label = QLabel("")
+		self.temps_restant_label.setFont(newfont)
+		self.length_label = QLabel("")
+		self.length_label.setFont(newfont)
+		
+		self.widget.layout().addWidget(self.name_label)
+		self.widget.layout().addWidget(self.length_label)
+		self.widget.layout().addWidget(self.progress_single)
+		self.widget.layout().addWidget(self.temps_restant_label)
+		self.widget.layout().addWidget(self.progress_group)
+		self.widget.layout().addWidget(self.video_total_label)
+		self.widget.layout().addWidget(self.quitButton)
+		
+		self.thread_dl = Downloader(to_dl_list)
+		self.thread_dl.start()
+		
+		
+		self.timer = QTimer()
+		self.timer.timeout.connect(self.update_percent)#self.thread_dl.join()
+		self.timer.start(500)
+		
+	def exit_app(self):
+		sys.exit()
 
-def cancel():
-	sys.exit()
-	
-	
-	
-def clearLayout(layout):
-	while layout.count():
-		child = layout.takeAt(0)
-		if child.widget() is not None:
-			child.widget().deleteLater()
-		elif child.layout() is not None:
-			clearLayout(child.layout())
+	def update_percent(self):
+		try:
+			self.name_label.setText(self.thread_dl.name)
+			self.length_label.setText(str(datetime.timedelta(seconds=self.thread_dl.length)))
+			percent_now = int((os.path.getsize("Dled/"+str(self.thread_dl.name)+".mp4.temp")/float(self.thread_dl.size))*100)
+			time_spent = round(time.time()-self.thread_dl.begin_time,0)
+			percent_left = 100-percent_now
+			time_left_estimated = time_spent/percent_now
+			time_left_estimated = int(time_left_estimated*percent_left)
 
-def ok(myWidgetList,myWidget):
-	to_dl_list = []
-	for i in range(len(myWidgetList)):
-		if myWidgetList[i].check.isChecked():
-			to_dl_list.append(myWidgetList[i].url)
-	print(to_dl_list)
-	print(myWidget.layout())
-	for i in reversed(range(myWidget.layout().count())): 
-		clearLayout(myWidget.layout())
-	progress_single = QProgressBar()
-	progress_group = QProgressBar()
-	progress_single.setValue(50)
-	
-	myWidget.layout().addWidget(progress_single)
-	myWidget.layout().addWidget(progress_group)
-	quitButton = QPushButton("Quit")
-	myWidget.layout().addWidget(quitButton)
-	quitButton.clicked.connect(exit_app)
-	
-	url = to_dl_list[0]
-	video = pafy.new(url)
-	best = video.getbest()
-	best.download(quiet=False)
-	
-def exit_app():
-	sys.exit()	
-	
+			self.video_total_label.setText("Video "+str(self.thread_dl.url_nbr+1)+"/"+str(self.thread_dl.video_total))
+			self.temps_restant_label.setText("Temps restant: Environ "+str(time_left_estimated)+" s")
+			self.progress_single.setValue(percent_now)
+			
+			self.progress_group.setValue((self.thread_dl.url_nbr)/float(self.thread_dl.video_total)*100)
+		except:
+			self.progress_single.setValue(100)
+			if(self.thread_dl.done):
+				self.progress_group.setValue(100)
+				self.quitButton.setEnabled(True)
 
+		if(self.thread_dl.done):
+			self.timer.stop()
+			self.clean_filename()
+			print()
+			print("Telechargement termine")
 	
+	def clean_filename(self):
+		file_list = os.listdir("/home/nemael/Desktop/Programmes/YoutubeDownloader/Dled/")
+		safe_char_list = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','-','(',')','[',']','+',' ','.','1','2','3','4','5','6','7','8','9','0']
+		for fil in range(len(file_list)):
+			name = file_list[fil]
+			i = 0
+			while i < len(name):
+				if name[i] in safe_char_list:
+					i += 1
+				else:
+					name = name[:i] + name[i+1:]
+			if name == ".mp4":
+				name = str(random.randint(1,10000))
+				name += ".mp4"
+			os.rename("/home/nemael/Desktop/Programmes/YoutubeDownloader/Dled/"+file_list[fil],"/home/nemael/Desktop/Programmes/YoutubeDownloader/Dled/"+name)
+				
 
 class WidgetVideo(QWidget):
-	def __init__(self, video_id, parent=None):
+	def __init__(self, video_id, length, parent=None):
 		QWidget.__init__(self, parent=parent)
 		
 		res_list = youtube_subs.get_videos_infos(video_id)
-		print(res_list)
+		print(res_list[0],res_list[3])
 		Hlay = QHBoxLayout(self)
 		self.url = "https://www.youtube.com/watch?v="+video_id
 		self.check = QCheckBox()
@@ -111,6 +196,8 @@ class WidgetVideo(QWidget):
 		if len(name) > 65:
 			name = name[:65] +"..."
 		Vlay.addWidget(QLabel(name))
+		Vlay.addWidget(QLabel(res_list[3]))
+		Vlay.addWidget(QLabel(str(datetime.timedelta(seconds=length))))
 		
 		
 		HlayIntern = QHBoxLayout()
@@ -125,8 +212,48 @@ class WidgetVideo(QWidget):
 		date = date[:10]+"\n"+date[11:19]
 		HlayIntern.addWidget(QLabel(date))
 		Vlay.addLayout(HlayIntern)
-		Vlay.addWidget(QLabel(res_list[3]))
 		
 		Hlay.addLayout(Vlay)
 		horizontalSpacer = QSpacerItem(20, 40, QSizePolicy.Expanding, QSizePolicy.Expanding)
 		Hlay.addItem(horizontalSpacer)
+		
+
+class Downloader(Thread):
+	def __init__(self,url_list):
+		Thread.__init__(self)
+		
+		self.url_list = url_list
+		self.video_total = len(url_list)
+		self.url_nbr = 0
+		self.url = url_list[0]
+		self.size = 0
+		self.name = ""
+		self.begin_time = 0
+		self.length = 0
+		self.done = False
+		
+		
+		
+	def run(self):
+		
+		for i in range(len(self.url_list)):
+			try:				
+				self.url_nbr = i
+				video = pafy.new(self.url_list[i])
+				best = video.getbest(preftype="mp4")
+				self.length = video.length
+				self.size = best.get_filesize()
+				self.name = video.title
+				self.begin_time = time.time()
+				
+				best.download(filepath = "Dled/",quiet=False)
+			except UnicodeEncodeError:
+				print("Erreur d'encodage sur la video",i)
+				video = pafy.new(self.url_list[i])
+				try:
+					print(video.title)
+				except:
+					print("Ne peut meme pas print le nom de la video")
+				best = video.getbest(preftype="mp4")
+				best.download(filepath = "Dled/",quiet=False)
+		self.done = True
--- debut.py
+++ debut.py
@@ -5,19 +5,8 @@ import json
 import youtube_subs
 import codecs
 import GUI
+import pafy
 
-#Mettre les videos dans l'ordre
-#Mettre un treshold pour eviter les vieilles videos
-
-"""test = youtube_search("Ninja")
-just_json = test[1]
-for video in just_json:
-	print video['snippet']['title']"""
-
-#print len(just_json)
-#print(just_json[0])
-#for video in just_json:
-#	print(video['snippet']['title'])
 def get_subs_list():
 	channels_name_list = []
 	channelsId_list = []
@@ -64,19 +53,6 @@ def update_watched_videos_list(video_list_depaqued):
 	f.close()
 	print("Dans fichier fait")
 
-"""def get_videos_already_watched(video_list):
-	list_from_file = []
-	watched = []
-	f = open('watched_videos.txt','r')
-	for line in f:
-		line = line.strip("https://www.youtube.com/watch?v=")
-		list_from_file.append(line)
-	f.close()
-	for i in video_list:
-		if i in list_from_file:
-			watched.append(i)
-	return(watched)"""
-
 def get_videos_not_watched(video_list):
 	list_from_file = []
 	not_watched = []
@@ -91,7 +67,7 @@ def get_videos_not_watched(video_list):
 			not_watched.append(i)
 	return(not_watched)
 
-print("Que voulez vous faire?\n(Treshold = 1 mois\n 1) Voir la liste des nouvelles videos\n 2) Updater la liste des abonnements\n 3) Mettre toutes les videos dans watched_videos.txt\n 4) Donnez les URL a la main pour telecharger les videos\n 5) Temp")
+print("Que voulez vous faire?\n(Treshold = 1 mois\n 1) Voir la liste des nouvelles videos\n 2) Updater la liste des abonnements\n 3) Mettre toutes les videos dans watched_videos.txt\n 4) Donnez les URL a la main pour telecharger les videos")
 choix = int(input())
 if choix == 1:
 	url_list = []
@@ -111,8 +87,7 @@ if choix == 1:
 	channels_file.close()
 
 	noms = []
-	for i in range(5):
-	#for i in range(len(channelsId_list)):
+	for i in range(len(channelsId_list)):
 		print(i,channelsname_list[i])
 		res = youtube_subs.search_date_user_id(channelsId_list[i])
 		for i in range(len(res)):
@@ -126,8 +101,12 @@ if choix == 1:
 		print("    ")
 	
 	url_not_watched = get_videos_not_watched(url_list)
-	url_to_dl = GUI.start(url_not_watched)
-	update_watched_videos_list(url_to_add)
+	print("Liste:")
+	for i in range(len(url_not_watched)):
+		print("https://www.youtube.com/watch?v="+url_not_watched[i])
+	print("Total: "+str(len(url_not_watched)))
+	update_watched_videos_list(url_not_watched)
+	gui = GUI.GUI(url_not_watched,False)
 		
 elif choix == 2:
 	channels_name, channelsId = get_subs_list()
@@ -157,10 +136,7 @@ elif choix == 3:
 	channels_file.close()
 
 	noms = []
-	for i in range(5):
-	#for i in range(len(channelsId_list)):
-		#for i in range(3):
-
+	for i in range(len(channelsId_list)):
 		print(i,channelsname_list[i])
 		res = youtube_subs.search_date_user_id(channelsId_list[i])
 		for i in range(len(res)):
@@ -177,8 +153,20 @@ elif choix == 3:
 	update_watched_videos_list(url_to_add)
 
 elif choix == 4:
-	print("Pas encore mis en place")
+	print("Veuillez entrer l'url des videos que vous voulez telecharger selon cette structure:\nhttps://www.youtube.com/watch?v=ID---------, autre chose lancera le telechargement")
+	url_list = []
+	stop = 0
+	while(not stop):
+		url = str(raw_input())
+		if len(url) == 43:
+			url_list.append(url[32:])
+		else:
+			stop = 1
+	print(url_list)
+	gui = GUI.GUI(url_list,True)
 
 elif choix == 5:
-	lis = ["_TH4SNjff40","uCawbVE6xPg","s7nFjhEf55U","h0MMUYvoZ94","JEA3LaLybDs","XvTvtwykNcY","Fff8CjDBYkA","swLBI9KLpTE","_C_CEhjqulc","mvunhIMXxpM"]
-	GUI.start(lis)
+	#lis = ["l7LG15O6L2Y"]
+	lis = ["_TH4SNjff40","s7nFjhEf55U","l7LG15O6L2Y"]
+	#lis = ["_TH4SNjff40","s7nFjhEf55U","uCawbVE6xPg","_TH4SNjff40","s7nFjhEf55U","_TH4SNjff40","s7nFjhEf55U","h0MMUYvoZ94"]
+	gui = GUI.GUI(lis,True)
